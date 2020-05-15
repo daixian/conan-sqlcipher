4c101dbeceb2e088c58da8f179e20aacaf337665
daixian <amano_tooko@qq.com>
2020-05-15 18:40:07 +0800

    增加了一些定义

--- conanfile.py
+++ conanfile.py
@@ -12,13 +12,14 @@ class SqlcipherConan(ConanFile):
     homepage = "https://www.zetetic.net/sqlcipher/"
     description = "SQLite extension that provides 256 bit AES encryption of database files."
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False], "crypto_library": ["openssl", "libressl"]}
-    default_options = {"shared": False, "fPIC": True, "crypto_library": "openssl"}
+    options = {"shared": [True, False], "fPIC": [
+        True, False], "crypto_library": ["openssl", "libressl"]}
+    default_options = {"shared": False,
+                       "fPIC": True, "crypto_library": "openssl"}
     topics = ("database", "encryption", "SQLite")
     exports_sources = "patches/*"
     generators = "cmake"
     _source_subfolder = "source_subfolder"
-
 
     def config_options(self):
         del self.settings.compiler.libcxx
@@ -46,17 +47,42 @@ class SqlcipherConan(ConanFile):
         crypto_dep = self.deps_cpp_info[str(self.options.crypto_library)]
         crypto_incdir = crypto_dep.include_paths[0]
         crypto_libdir = crypto_dep.lib_paths[0]
-        libs = map(lambda lib : lib + ".lib", crypto_dep.libs)
+        libs = map(lambda lib: lib + ".lib", crypto_dep.libs)
 
         nmake_flags = [
-                "TLIBS=\"%s\"" % " ".join(libs),
-                "LTLIBPATHS=/LIBPATH:%s" % crypto_libdir,
-                "OPTS=\"-I%s -DSQLITE_HAS_CODEC -DSQLITE_ENABLE_COLUMN_METADATA\"" % (crypto_incdir),
-                "NO_TCL=1",
-                "USE_AMALGAMATION=1",
-                "OPT_FEATURE_FLAGS=-DSQLCIPHER_CRYPTO_OPENSSL",
-                "TCLSH_CMD=%s" % self.deps_env_info.TCLSH,
-                ]
+            "TLIBS=\"%s\"" % " ".join(libs),
+            "LTLIBPATHS=/LIBPATH:%s" % crypto_libdir,
+            "OPTS=\"-I%s -DSQLITE_HAS_CODEC \
+-DSQLITE_DQS=0 \
+-DSQLITE_MAX_ATTACHED=10 \
+-DSQLITE_ENABLE_EXPLAIN_COMMENTS \
+-DSQLITE_SOUNDEX \
+-DSQLITE_ENABLE_COLUMN_METADATA \
+-DSQLITE_SECURE_DELETE \
+-DSQLITE_ENABLE_FTS3 \
+-DSQLITE_ENABLE_FTS3_PARENTHESIS \
+-DSQLITE_ENABLE_FTS4 \
+-DSQLITE_ENABLE_FTS5 \
+-DSQLITE_ENABLE_JSON1 \
+-DSQLITE_ENABLE_RTREE \
+-DSQLITE_ENABLE_GEOPOLY \
+-DSQLITE_CORE \
+-DSQLITE_ENABLE_EXTFUNC \
+-DSQLITE_ENABLE_CSV \
+-DSQLITE_ENABLE_SHA3 \
+-DSQLITE_ENABLE_CARRAY \
+-DSQLITE_ENABLE_FILEIO \
+-DSQLITE_ENABLE_SERIES \
+-DSQLITE_ENABLE_UUID \
+-DSQLITE_TEMP_STORE=2 \
+-DSQLITE_USE_URI \
+\"" % (crypto_incdir),
+            # -DSQLITE_USER_AUTHENTICATION \ 暂时不能加
+            "NO_TCL=1",
+            "USE_AMALGAMATION=1",
+            'OPT_FEATURE_FLAGS="-DSQLCIPHER_CRYPTO_OPENSSL"',
+            "TCLSH_CMD=%s" % self.deps_env_info.TCLSH,
+        ]
 
         main_target = "dll" if self.options.shared else "sqlcipher.lib"
 
@@ -68,16 +94,43 @@ class SqlcipherConan(ConanFile):
         platforms = {"x86": "x86", "x86_64": "x64"}
         nmake_flags.append("PLATFORM=%s" % platforms[self.settings.arch.value])
         vcvars = tools.vcvars_command(self.settings)
-        self.run("%s && nmake /f Makefile.msc %s %s" % (vcvars, main_target, " ".join(nmake_flags)), cwd=self._source_subfolder)
+        self.run("%s && nmake /f Makefile.msc %s %s" % (vcvars,
+                                                        main_target, " ".join(nmake_flags)), cwd=self._source_subfolder)
 
     def _build_autotools(self):
         self.run('chmod +x configure', cwd=self._source_subfolder)
         absolute_install_dir = os.path.abspath(os.path.join(".", "install"))
         absolute_install_dir = absolute_install_dir.replace("\\", "/")
-        autotools_env = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        autotools_env = AutoToolsBuildEnvironment(
+            self, win_bash=tools.os_info.is_windows)
         if self.settings.os == "Linux":
             autotools_env.libs.append("dl")
-        autotools_env.defines.extend(["SQLITE_HAS_CODEC", "SQLCIPHER_CRYPTO_OPENSSL","SQLITE_ENABLE_COLUMN_METADATA"])
+        autotools_env.defines.extend(
+            ["SQLITE_HAS_CODEC", "SQLCIPHER_CRYPTO_OPENSSL",
+             "SQLITE_DQS=0",
+             "SQLITE_MAX_ATTACHED=10",
+             "SQLITE_ENABLE_EXPLAIN_COMMENTS",
+             "SQLITE_SOUNDEX",
+             "SQLITE_ENABLE_COLUMN_METADATA",
+             "SQLITE_SECURE_DELETE",
+             "SQLITE_ENABLE_FTS3",
+             "SQLITE_ENABLE_FTS3_PARENTHESIS",
+             "SQLITE_ENABLE_FTS4",
+             "SQLITE_ENABLE_FTS5",
+             "SQLITE_ENABLE_JSON1",
+             "SQLITE_ENABLE_RTREE",
+             "SQLITE_ENABLE_GEOPOLY",
+             "SQLITE_CORE",
+             "SQLITE_ENABLE_EXTFUNC",
+             "SQLITE_ENABLE_CSV",
+             "SQLITE_ENABLE_SHA3",
+             "SQLITE_ENABLE_CARRAY",
+             "SQLITE_ENABLE_FILEIO",
+             "SQLITE_ENABLE_SERIES",
+             "SQLITE_ENABLE_UUID",
+             "SQLITE_TEMP_STORE=2",
+             "SQLITE_USE_URI"])
+             #"SQLITE_USER_AUTHENTICATION" 不能使用
 
         # sqlcipher config.sub does not contain android configurations...
         # elf is the most basic `os' for Android
@@ -105,10 +158,12 @@ class SqlcipherConan(ConanFile):
                 env_vars["config_TARGET_EXEEXT"] = ".exe"
             else:
                 build = None
-            autotools_env.configure(args=configure_args, host=host, build=build, vars=env_vars)
+            autotools_env.configure(
+                args=configure_args, host=host, build=build, vars=env_vars)
             if self.settings.os == "Windows":
                 # sqlcipher will create .exe for the build machine, which is Linux...
-                tools.replace_in_file(os.path.join(self.build_folder, self._source_subfolder, "Makefile"), "BEXE = .exe", "BEXE = ")
+                tools.replace_in_file(os.path.join(
+                    self.build_folder, self._source_subfolder, "Makefile"), "BEXE = .exe", "BEXE = ")
             autotools_env.make(args=["install"])
 
     @property
@@ -132,7 +187,8 @@ class SqlcipherConan(ConanFile):
             "--with-tcl={}".format(os.path.join(self.deps_env_info.TCL_ROOT, "lib"))
         ]
         if self.settings.os == "Windows":
-            args.extend(["config_BUILD_EXEEXT='.exe'", "config_TARGET_EXEEXT='.exe'"])
+            args.extend(["config_BUILD_EXEEXT='.exe'",
+                         "config_TARGET_EXEEXT='.exe'"])
         return args
 
     def _autotools_bool_arg(self, arg_base_name, value):
@@ -144,7 +200,8 @@ class SqlcipherConan(ConanFile):
         for patch in self.conan_data["patches"][self.version]:
             tools.patch(**patch)
         if self.settings.os == "Macos":
-            tools.replace_in_file(os.path.join(self._source_subfolder, "configure"), r"-install_name \$rpath/", "-install_name ")
+            tools.replace_in_file(os.path.join(
+                self._source_subfolder, "configure"), r"-install_name \$rpath/", "-install_name ")
 
         if self.settings.compiler == "Visual Studio":
             self._build_visual()
@@ -153,7 +210,8 @@ class SqlcipherConan(ConanFile):
 
     def _package_unix(self):
         self.copy("*sqlite3.h", src="install")
-        self.copy("*.so*", dst="lib", src="install", keep_path=False, symlinks=True)
+        self.copy("*.so*", dst="lib", src="install",
+                  keep_path=False, symlinks=True)
         self.copy("*.a", dst="lib", src="install", keep_path=False)
         self.copy("*.lib", dst="lib", src="install", keep_path=False)
         self.copy("*.dll", dst="bin", src="install", keep_path=False)
@@ -164,7 +222,8 @@ class SqlcipherConan(ConanFile):
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*LICENSE", dst="licenses", keep_path=False)
-        self.copy("sqlite3.h", src=self._source_subfolder, dst=os.path.join("include", "sqlcipher"))
+        self.copy("sqlite3.h", src=self._source_subfolder,
+                  dst=os.path.join("include", "sqlcipher"))
 
     def package(self):
         if self.settings.compiler == "Visual Studio":
@@ -176,6 +235,30 @@ class SqlcipherConan(ConanFile):
         self.cpp_info.libs = ["sqlcipher"]
         if self.settings.os == "Linux":
             self.cpp_info.system_libs.extend(["pthread", "dl"])
-        self.cpp_info.defines = ["SQLITE_HAS_CODEC", 'SQLCIPHER_CRYPTO_OPENSSL', 'SQLITE_TEMP_STORE=2', "SQLITE_ENABLE_COLUMN_METADATA"]
+        self.cpp_info.defines = ["SQLITE_HAS_CODEC",
+                                 'SQLCIPHER_CRYPTO_OPENSSL',
+                                 "SQLITE_DQS=0",
+                                 "SQLITE_MAX_ATTACHED=10",
+                                 "SQLITE_ENABLE_EXPLAIN_COMMENTS",
+                                 "SQLITE_SOUNDEX",
+                                 "SQLITE_ENABLE_COLUMN_METADATA",
+                                 "SQLITE_SECURE_DELETE",
+                                 "SQLITE_ENABLE_FTS3",
+                                 "SQLITE_ENABLE_FTS3_PARENTHESIS",
+                                 "SQLITE_ENABLE_FTS4",
+                                 "SQLITE_ENABLE_FTS5",
+                                 "SQLITE_ENABLE_JSON1",
+                                 "SQLITE_ENABLE_RTREE",
+                                 "SQLITE_ENABLE_GEOPOLY",
+                                 "SQLITE_CORE",
+                                 "SQLITE_ENABLE_EXTFUNC",
+                                 "SQLITE_ENABLE_CSV",
+                                 "SQLITE_ENABLE_SHA3",
+                                 "SQLITE_ENABLE_CARRAY",
+                                 "SQLITE_ENABLE_FILEIO",
+                                 "SQLITE_ENABLE_SERIES",
+                                 "SQLITE_ENABLE_UUID",
+                                 "SQLITE_TEMP_STORE=2",
+                                 "SQLITE_USE_URI"]
         # Allow using #include <sqlite3.h> even with sqlcipher (for libs like sqlpp11-connector-sqlite3)
         self.cpp_info.includedirs.append(os.path.join("include", "sqlcipher"))
--- test_package/example.c
+++ test_package/example.c
@@ -1,9 +1,27 @@
 #include <sqlcipher/sqlite3.h>
 
-int main() {
-  sqlite3* db;
-  sqlite3_open(":memory:", &db);
+int sqlite_callback(
+    void *pv,    /* 由 sqlite3_exec() 的第四个参数传递而来 */
+    int argc,    /* 表的列数 */
+    char **argv, /* 指向查询结果的指针数组, 可以由 sqlite3_column_text() 得到 */
+    char **col   /* 指向表头名的指针数组, 可以由 sqlite3_column_name() 得到 */
+)
+{
+}
+
+int main()
+{
+  remove("test.db3");
+  char *zErrMsg = NULL;
+  sqlite3 *db;
+  // sqlite3_open(":memory:", &db);
+  sqlite3_open("test.db3", &db);
   int ret = sqlite3_key(db, "toto", 5);
+
+  sqlite3_exec(db,
+               "CREATE TABLE sys_info (id INTEGER PRIMARY KEY, dev_id INTEGER, dev_id_byte BLOB, dev_id_base64 TEXT, dev_ver TEXT, soft_ver TEXT, calib_ver TEXT)",
+               sqlite_callback, NULL, zErrMsg);
+
   sqlite3_close(db);
   return ret;
 }
